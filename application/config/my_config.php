040b0bd9df843f74201ce4a0a930be1536f13854
atuvshinjargal <53771399+atuvshinjargal@users.noreply.github.com>
2019-08-14 23:15:16 +0800

    First commit

---
+++ application/config/my_config.php
@@ -0,0 +1,22 @@
+<?php 
+$config['default_page_url'] = 'page/blank';
+$config['product_name'] = 'Технологийн сургууль';
+$config['product_short_name'] = 'Технологийн сургууль' ;
+$config['product_version'] = ' v2.6.2  ';
+
+$config['institute_address1'] = 'Ш.Отгонбилэгийн нэрэмжит Технологийн сургууль';
+$config['institute_address2'] = 'Орхон аймаг Эрдэнэт хот';
+$config['institute_email'] = 'techinst@erdenetmc.mn';
+$config['institute_mobile'] = '-97695348002';
+
+$config['developed_by'] = ' А.Түвшинжаргал ';
+$config['developed_by_href'] = 'http://erdenetis.edu.mn';
+$config['developed_by_title'] = 'Эрчим хүч, мэдээллийн технологийн тэнхим';
+$config['developed_by_prefix'] = 'Хөгжүүлсэн' ;
+$config['support_email'] = 'tuvshinjargal.a@erdenetmc.mn' ;
+$config['support_mobile'] = '+976 99562046' ;
+$config['time_zone'] = 'Asia/Ulaanbaatar';
+$config['currency'] = 'USD';
+$config['language'] = 'english';
+$config['sess_use_database'] = true;
+$config['sess_table_name'] = 'ci_sessions';
